8dbbedd36f05a6d9369300b48a5c951859c6726e
Wissam <aitkheddachewissam@gmail.com>
2022-10-30 20:37:05 +0100

    site personalisation

--- data/constants.ts
+++ data/constants.ts
@@ -9,4 +9,4 @@ export const DEVTO_API_URL = 'https://dev.to/api';
 /**
  * SEO constants
  */
-export const SEO_IMAGE = 'https://mahmad.me/logo.png';
+export const SEO_IMAGE = 'https://media-exp1.licdn.com/dms/image/C4E03AQHTYqBNmq7g3w/profile-displayphoto-shrink_200_200/0/1663337757751?e=1672876800&v=beta&t=1dKw8xG1KCLZKg4nI-FRJyViDcVwJtXPSL8O7U6hIQA';
